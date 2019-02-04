d083db41c3acebd732556c204b294dafcb75da5a
geoffroyvilain <vilaingeoffroy02@gmail.com>
2019-02-04 10:54:17 +0100

    exo fini

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+$(function(){
+   $('#show').click(function(){ // on appelle la fonction au click
+   $('#text').show();           // elle s'applique au paragraphe 'text' et se montre
+  $('#hide').click(function(){  // on appelle la fonction au click
+  $('#text').hide();            // elle s'applique au paragraphe 'text' et se montre
+});
+});
+});
